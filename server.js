2550969562cccf0f14be5d56ace2e1f750cd0776
portapro <uvarenkov@gmail.com>
2017-04-28 13:24:29 +0300

    #1635

--- server.js
+++ server.js
@@ -24,33 +24,33 @@ var pathToQoobHtml = 'data/html/qoob.html';
 var pathToQoobDashboard = 'data/html/dashboard.html';
 
 function onRequest(req, res) {
-    
+
     var currentUrl = url.parse(req.url, true);
 
-	if (currentUrl.pathname === "/qoob/"){
-		res.writeHead(302, {
-		  'Location': '/qoob'
-		});
-		res.end();
-		return;
-	}
+    if (currentUrl.pathname === "/qoob/") {
+        res.writeHead(302, {
+            'Location': '/qoob'
+        });
+        res.end();
+        return;
+    }
 
     if (currentUrl.pathname === "/qoob") {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        var content ='<table>';
+        var content = '<table>';
         fs.readdir(pathToPageData, function(err, files) {
             for (var i = 0; i < files.length; i++) {
-                if(files[i]!=='empty.json'){
+                if (files[i] !== 'empty.json') {
                     page = files[i].replace('.json', '');
-                    content=content+'<tr><td>'+page+'.html</td><td><a href="'+page+'.html">View</a></td><td><a href="'+page+'.html?edit">Edit</a></td><td>'+(page!='index'?'<a href="'+page+'.html?delete" class="delete">Delete</a>':'')+'</td></tr>';
+                    content = content + '<tr><td class="name">' + page + '.html</td><td class="view"><a href="' + page + '.html"><i class="fa fa-eye"></i> <span class="text">View</span></a></td><td class="edit"><a href="' + page + '.html?edit"><i class="fa fa-pencil"></i> <span class="text">Edit</span></a></td><td class="delete">' + (page != 'index' ? '<a href="' + page + '.html?delete" class="delete"><i class="fa fa-remove"></i> <span class="text">Delete</span></a>' : '') + '</td></tr>';
                 }
             }
-            content=content+'</table>';
-            fs.readFile(pathToQoobDashboard, 'utf8', function (err, data) {
+            content = content + '</table>';
+            fs.readFile(pathToQoobDashboard, 'utf8', function(err, data) {
                 res.write(data.replace("<!-- qoob pages list -->", content));
                 res.end();
-            });        
+            });
 
         });
         return;
@@ -59,30 +59,30 @@ function onRequest(req, res) {
     if (currentUrl.pathname === "/layout.html") {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        fs.readFile(pathToLayout, 'utf8', function (err, data) {
+        fs.readFile(pathToLayout, 'utf8', function(err, data) {
             data = data.replace("<!-- qoob starter -->", '<script type="text/javascript" src="assets/js/qoob-html-driver.js"></script><script type="text/javascript" src="qoob/qoob-frontend-starter.js"></script><script type="text/javascript">var starter = new QoobStarter({"driver": new QoobHtmlDriver()});</script>');
             res.write(data);
             res.end();
-        });        
+        });
         return;
     }
-    
+
     if (typeof currentUrl.query.edit !== 'undefined') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        fs.readFile(pathToQoobHtml, 'utf8', function (err, data) {
+        fs.readFile(pathToQoobHtml, 'utf8', function(err, data) {
             res.write(data);
             res.end();
-        });        
+        });
         return;
     }
 
     if (typeof currentUrl.query.delete !== 'undefined') {
-        var page = currentUrl.pathname.replace('.html','').replace('/','');
-        fs.unlink(pathToPageData+page+".json");
-        fs.unlink(page+".html");
+        var page = currentUrl.pathname.replace('.html', '').replace('/', '');
+        fs.unlink(pathToPageData + page + ".json");
+        fs.unlink(page + ".html");
         res.writeHead(302, {
-          'Location': '/qoob'
+            'Location': '/qoob'
         });
         res.end();
         return;
@@ -101,7 +101,7 @@ function onRequest(req, res) {
             req.on('end', function() {
                 var data = JSON.parse(body);
                 //SAVE page data
-                fs.writeFile(pathToPageData+data.page+".json", JSON.stringify(data.data), function(err) {
+                fs.writeFile(pathToPageData + data.page + ".json", JSON.stringify(data.data), function(err) {
                     if (err) {
                         return console.log(err);
                     }
@@ -124,23 +124,23 @@ function onRequest(req, res) {
                         for (var j = 0; j < data.libs[i].res.length; j++) {
                             if (data.libs[i].res[j].src.indexOf("http://") !== 0 && data.libs[i].res[j].src.indexOf("https://") !== 0) {
                                 data.libs[i].res[j].src = libUrl + data.libs[i].res[j].src.replace(/^\/+/g, ''); //Trim slashes in the begining
-                            }                            
+                            }
                             loader.add(data.libs[i].res[j]);
                         }
                     }
 
-                    loader.loadJS = function(src, success, error){
-                        scripts = scripts + '\n\r<script type="text/javascript" src="'+src+'"></script>';
+                    loader.loadJS = function(src, success, error) {
+                        scripts = scripts + '\n\r<script type="text/javascript" src="' + src + '"></script>';
                         success();
                     };
-                    loader.loadCSS = function(src, success, error){
-                        css=css+'\n\r<link rel="stylesheet" href="'+src+'">';
+                    loader.loadCSS = function(src, success, error) {
+                        css = css + '\n\r<link rel="stylesheet" href="' + src + '">';
                         success();
                     };
-                    loader.on('complete', function(){
-                        fileData = fileData.replace("</head>", css+scripts + "\n\r</head>");
+                    loader.on('complete', function() {
+                        fileData = fileData.replace("</head>", css + scripts + "\n\r</head>");
 
-                        fs.writeFile(data.page+".html", fileData, function(err) {
+                        fs.writeFile(data.page + ".html", fileData, function(err) {
                             if (err) {
                                 return console.log(err);
                             }
@@ -244,14 +244,14 @@ function onRequest(req, res) {
 
             fs.createReadStream(filePath).pipe(res)
         } else {
-            if (currentUrl.pathname.lastIndexOf('/'+pathToPageData, 0) === 0){
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'text/html');
-                    fs.readFile(pathToEmptyPageData, 'utf8', function (err, data) {
-                        res.write(data);
-                        res.end();
-                    });        
-                    return;    
+            if (currentUrl.pathname.lastIndexOf('/' + pathToPageData, 0) === 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.readFile(pathToEmptyPageData, 'utf8', function(err, data) {
+                    res.write(data);
+                    res.end();
+                });
+                return;
             }
 
             // if not, then we set our status code to 404, and send an error page
@@ -267,9 +267,9 @@ function onRequest(req, res) {
         var now = new Date();
         if (res.statusCode != 200) {
             console.log(now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(),
-                    '-', req.method,
-                    '-', req.url,
-                    '-', res.statusCode)
+                '-', req.method,
+                '-', req.url,
+                '-', res.statusCode)
         }
     })
 }
