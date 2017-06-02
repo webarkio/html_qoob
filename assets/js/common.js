77dfce76926db9c998052377b34c3b3717fe2c2d
portapro <uvarenkov@gmail.com>
2017-06-02 14:36:13 +0300

    add simple skin

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,28 +1,42 @@
-jQuery(document).ready(function(){
-	// Init masonry blog list
-  jQuery('.blog-list').masonry({
-    // columnWidth: '.col-lg-4',
-	  itemSelector: '.col-lg-4',
-	  percentPosition: true,
-	  resize: true
-	});
+jQuery(document).ready(function() {
+    // Init masonry blog list
+    jQuery('.blog-list').masonry({
+        // columnWidth: '.col-lg-4',
+        itemSelector: '.col-lg-4',
+        percentPosition: true,
+        resize: true
+    });
 
-	window.onscroll = function() {
-      if (window.pageYOffset > 107) {
-        jQuery('.site-header.fixed').addClass('move');
-      } else {
-      	jQuery('.site-header.fixed').removeClass('move');
-      }
-  };
-  
-  jQuery(window).resize(function(){
-    jQuery('.blog-list').masonry('reloadItems');
-  });
+    window.onscroll = function() {
+        if (window.pageYOffset > 107) {
+            jQuery('.site-header.fixed').addClass('move');
+        } else {
+            jQuery('.site-header.fixed').removeClass('move');
+        }
+    };
+
+    jQuery(window).resize(function() {
+        jQuery('.blog-list').masonry('reloadItems');
+    });
+
+    var hammer = new Hammer.Manager(document.documentElement);
+    var swipe = new Hammer.Swipe({ direction: Hammer.DIRECTION_HORIZONTAL });
+    hammer.add(swipe);
+    hammer.on("swiperight swipeleft", function(e) {
+        if (e.type === 'swiperight') {
+          console.log('swiperight');
+            parent.postMessage("SwipeRightPageMessage", "*");
+        } else if (e.type === 'swipeleft') {
+          console.log('swipeleft');
+            parent.postMessage("SwipeLeftPageMessage", "*");
+        }
+    });
 });
+
+
 
 // Loader fading out
 jQuery(window).load(function() {
     jQuery(".loader").fadeOut();
     jQuery(".loader-wrap").delay(400).fadeOut("slow");
 });
-
